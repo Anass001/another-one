f7834e27bbdba7ca28297a2c0da0ae4c2eed5339
Nas <lamaizanass@gmail.com>
2018-12-26 22:44:25 +0100

    vbo

--- app/src/main/java/com/zeneo/turkeydramaapp/MainActivity.java
+++ app/src/main/java/com/zeneo/turkeydramaapp/MainActivity.java
@@ -5,8 +5,16 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
-
+import android.widget.Toast;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -15,35 +23,80 @@ import Item.ListItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ArrayList<String> titles;
+    private ArrayList<String> linkslist;
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
     private List <ListItem> listItems;
     private ArrayList arrayList;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getWebsite();
+
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
         getSupportActionBar().setElevation(0);
         getSupportActionBar().setTitle("");
+    }
 
-        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    private void getWebsite(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Document doc = Jsoup.connect("http://33sk.tv/vb/").get();
 
-        listItems = new ArrayList<>();
-        List<String> list = Arrays.asList(getResources().getStringArray(R.array.recycle_title));
+                    String title = doc.title();
 
-        for(int i=0; i<10 ;i++){
-            ListItem item = new ListItem(list.get(i),"Coco",R.drawable.poster1);
-            listItems.add(item);
-        }
+                    Elements links = doc.select("td[class=alt1Active]").select("a[href]");
 
-        adapter = new MyAdapter(this, listItems);
-        recyclerView.setAdapter(adapter);
+                    titles = new ArrayList<>();
+                    linkslist = new ArrayList<>();
 
+                    for(Element link : links){
+                        titles.add(link.text());
+                        linkslist.add(link.attr("href"));
+                    }
+
+
+                } catch (IOException e) {
+                    Toast.makeText(MainActivity.this, "DIFO AKHAY TABI3A",
+                            Toast.LENGTH_LONG).show();
+                }
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        titles.remove(0);
+                        linkslist.remove(0);
+                        titles.remove(titles.size()-1);
+                        linkslist.remove(linkslist.size()-1);
+                        titles.remove(titles.size()-1);
+                        linkslist.remove(linkslist.size()-1);
+
+                        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+                        recyclerView.setHasFixedSize(true);
+                        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+
+                        listItems = new ArrayList<>();
+
+                        for(int i=0; i<titles.size() ;i++){
+                            if(titles!= null && titles.size() !=0) {
+                                ListItem item = new ListItem(titles.get(i),linkslist.get(i),R.drawable.poster1);
+                                listItems.add(item);
+                            }
+                        }
+
+                        adapter = new MyAdapter(MainActivity.this, listItems);
+                        recyclerView.setAdapter(adapter);
+                    }
+                });
+            }
+        }).start();
     }
 }
